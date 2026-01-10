29eb1b3ea32e1f21928b60700b54c19666cfbfcc
Avinash Niyas <avinashNiyaz1423@gmail.com>
2026-01-11 01:32:28 +0530

    car movement adjusted

--- car/src/Car.js
+++ car/src/Car.js
@@ -9,10 +9,10 @@ export default class Car{
         this.width=width;
         this.height=height;
         this.speed=0;
-        this.accelaration=0.3
+        this.accelaration=0.3;
         this.maxSpeed=3;
-   
-
+        this.angle=0;
+      
         this.control=new Control();
     }
 
@@ -22,14 +22,20 @@ export default class Car{
         if(this.control.forward) this.speed+=this.accelaration;
         if(this.control.reverse) this.speed-=this.accelaration;
 
-        
+        let flip=1;
+        if(this.speed<0) flip=-1;
+
+         if(this.control.left) this.angle-=0.03*flip;
+         if(this.control.right) this.angle+=0.03*flip;
+                                 
+    
         if(this.speed>this.maxSpeed) this.speed=this.maxSpeed;
         if(this.speed<-this.maxSpeed/2) this.speed= -this.maxSpeed/2;
 
-           this.y-=this.speed;
+         this.y-=Math.cos(this.angle)*this.speed;
+         this.x+=Math.sin(this.angle)*this.speed;
+             
 
-        if(this.control.left) this.x-=2;
-        if(this.control.right) this.x+=2; 
     }
 
 
@@ -38,14 +44,17 @@ export default class Car{
         ctx.save();
         ctx.beginPath();
         ctx.translate(this.x,this.y);
+        ctx.rotate(this.angle);
         ctx.rect(
              -this.width/2,
              -this.height/2,
               this.width,
               this.height
         );
+        ctx.fillStyle="blue"
         ctx.fill();
         ctx.restore();
+        
 
     }
 
